c36b4888436e34621c2b15a6c9897a889b8e800b
N30-Geek <dryikobu967@gmail.com>
2025-09-04 20:48:33 +0200

    adding the hash function for password in sha256 algorithm

--- helper/functions/functions.py
+++ helper/functions/functions.py
@@ -1,5 +1,6 @@
 import re 
-
+from hashlib import sha256
+#===============================================
 def check_password(pwd: str)-> bool:
     # This function checking the valide password
     password_regex = r"(?=.*[a-zA-Z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%#?&])[A-Za-z\d@$!%#?&]{8,}$"
@@ -8,6 +9,7 @@ def check_password(pwd: str)-> bool:
         return True 
     else: return False
 
+#===============================================
 def check_email(pwd: str)-> bool:
     # this function checking the valid email
     email_regex = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
@@ -16,14 +18,15 @@ def check_email(pwd: str)-> bool:
         return True 
     else: return False
 
+#===============================================
+def hash_password(pwd: str) -> str:
+    #this function hash and return the clear password passed in parameter
+
+    clear_password = b""+pwd.encode("utf-8")
+    return sha256(clear_password).hexdigest()
+
+
 # ====  testing code 
 
-# if __name__  == "__main__":
-#     list_of_email = ['dryikobu987@gmia.com', 'passw.com', 'password1^@passwor.com', 'password@emi.com', 'password@']
-#     list_of_password = ['1289878892', 'kjkskjkls', 'kdslklksld2898', 'drykdKK88', 'KJkdslk9880@?']
-
-#     for email in list_of_email:
-#         print(f"Is this a valide email {email} ?  : {check_email(email)}")
-#     print("*"*50)
-#     for password in list_of_password:
-#         print(f"Is this valide password {password} ? {check_password(password)}")
+if __name__ == "__main__":
+    print(hash_password("Hello world"))
